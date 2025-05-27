c2ea81d116fada6763638d2ed6751e41462d0e39
exotic <michael303atkinson@gmail.com>
2025-05-27 18:14:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from routes.public import public_bp
 from routes.student import student_bp
 from routes.admin import admin_bp
 
-app = Flask(name)
+app = Flask(__name__)
+
 app.secret_key = "your-secret-key"
 
 
@@ -12,5 +13,5 @@ app.register_blueprint(public_bp)
 app.register_blueprint(student_bp)
 app.register_blueprint(admin_bp)
 
-if name == "main":
+if __name__ == "main":
     app.run(debug=True)
